cc2bddc4e990d26c9763678e5cf6161ca8d086e5
Alon Zakai <azakai@google.com>
2021-12-01 11:35:09 -0800

    work

--- src/passes/FuncEffects.cpp
+++ src/passes/FuncEffects.cpp
@@ -28,12 +28,41 @@ namespace wasm {
 
 struct GenerateFuncEffects : public WalkerPass<PostWalker<GenerateFuncEffects>> {
   virtual void run(PassRunner* runner, Module* module) {
-    struct Info {
-    };
+    using Info = std::shared_ptr<EffectAnalyzer>;
+
+    // Create a single Info to represent "anything" - any effect might happen,
+    // and we give up on trying to analyze things. To represent that, mark it as
+    // doing a call (which could do anything in the called code). Note that this
+    // does not say anything about effects on locals on the stack, which is
+    // intentional - we will use this as the effects of a call, which indeed
+    // cannot have such effects.
+    Info anything = std::make_shared<EffectAnalyzer>(runner->options, *module);
+    anything->calls = true;
 
     ParallelFunctionAnalysis<Info> analysis(*module, [&](Function* func, Info& info) {
+      if (func->imported()) {
+        // Imported functions can do anything.
+        info = anything;
+      } else {
+        // For defined functions, compute the effects in their body.
+        info = std::make_shared<EffectAnalyzer>(runner->options, *module, func->body);
+
+        // Discard any effects on locals, since those are not noticeable in the
+        // caller.
+        info->localsWritten.clear();
+        info->localsRead.clear();
+      }
     });
 
+    // TODO: Propagate effects through direct calls. Without that we only look
+    //       one call deep, basically.
+
+    // Copy the info to the final location.
+    auto& funcEffects = runner->options.funcEffects;
+    funcEffects.clear();
+    for (auto& [func, info] : analysis.map) {
+      funcEffects[func->name] = info;
+    }
   }
 };
 
