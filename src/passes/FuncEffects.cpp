b574f4c829c62dc97a7910b0805a033c5c3dc3b3
Alon Zakai <azakai@google.com>
2021-12-01 12:04:56 -0800

    frist working

--- src/passes/FuncEffects.cpp
+++ src/passes/FuncEffects.cpp
@@ -28,16 +28,23 @@ namespace wasm {
 
 struct GenerateFuncEffects : public WalkerPass<PostWalker<GenerateFuncEffects>> {
   virtual void run(PassRunner* runner, Module* module) {
+    // First, clear any previous function effects. We don't want to notice them
+    // when we compute effects here.
+    auto& funcEffects = runner->options.funcEffects;
+    funcEffects.clear();
+
     using Info = std::shared_ptr<EffectAnalyzer>;
 
     // Create a single Info to represent "anything" - any effect might happen,
-    // and we give up on trying to analyze things. To represent that, mark it as
-    // doing a call (which could do anything in the called code). Note that this
+    // and we give up on trying to analyze things. To represent that, scan a
+    // fake call (running the actual effect analyzer code on a call is important
+    // so that it picks up things like possibly throwing if exceptions are
+    // enabled, etc.). Note that this
     // does not say anything about effects on locals on the stack, which is
     // intentional - we will use this as the effects of a call, which indeed
     // cannot have such effects.
-    Info anything = std::make_shared<EffectAnalyzer>(runner->options, *module);
-    anything->calls = true;
+    Call fakeCall(module->allocator);
+    Info anything = std::make_shared<EffectAnalyzer>(runner->options, *module, &fakeCall);
 
     ModuleUtils::ParallelFunctionAnalysis<Info> analysis(*module, [&](Function* func, Info& info) {
       if (func->imported()) {
@@ -51,15 +58,28 @@ struct GenerateFuncEffects : public WalkerPass<PostWalker<GenerateFuncEffects>> 
         // caller.
         info->localsWritten.clear();
         info->localsRead.clear();
+
+        // Discard branching out of an expression or a return - we are returning
+        // back out to the caller anyhow.
+        info->branchesOut = false;
+
+        // As we have parsed an entire function, there should be no structural
+        // info about being inside a try-catch.
+        assert(!info->tryDepth);
+        assert(!info->catchDepth);
+        assert(!info->danglingPop);
       }
     });
 
     // TODO: Propagate effects through direct calls. Without that we only look
     //       one call deep, basically.
+    //       The propagation can use the identity of |anything| for convenience
+    //       (to indicate "we've failed to learn anything here").
+
+    // TODO: share the Info object between functions where possible to save
+    //       memory, like we do with |anything| already.
 
     // Copy the info to the final location.
-    auto& funcEffects = runner->options.funcEffects;
-    funcEffects.clear();
     for (auto& [func, info] : analysis.map) {
       funcEffects[func->name] = info;
     }
