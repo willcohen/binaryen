8b0ead263cc6f111239ce0cd3728a60fd2e78e93
Ashley Nelson <nashley@google.com>
2022-06-20 06:38:23 +0000

    src/passes

--- src/passes/Print.cpp
+++ src/passes/Print.cpp
@@ -3074,7 +3074,7 @@ struct PrintSExpression : public UnifiedExpressionVisitor<PrintSExpression> {
       doIndent(o, indent);
       o << '(';
       emitImportHeader(curr);
-      printMemoryHeader(&currModule->memory);
+      printMemoryHeader(&currModule->memories[0]);
       o << ')' << maybeNewLine;
     } else {
       doIndent(o, indent);
